42fd2c385f3fd26416aefce6f2792e6b5948e1ee
Anastacia Muringo <anastasmuringo@gmail.com>
2022-07-14 12:23:57 +0300

    Previous mod not working

--- script.js
+++ script.js
@@ -9,19 +9,17 @@ function getApod() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      if (date === data.date) {
-        document.querySelector('span').innerText =
-          document.querySelector('input').value;
-        //create a conditional to accomodate video media
-        if (data.media_type === 'video') {
-          document.querySelector('iframe').classList.remove('hidden');
-          document.querySelector('iframe').src = data.url;
-        } else {
-          document.querySelector('img').classList.remove('hidden');
-          document.querySelector('img').src = data.hdurl;
-        }
-        document.querySelector('p').innerText = data.explanation;
-      } else return;
+      document.querySelector('span').innerText =
+        document.querySelector('input').value;
+      //create a conditional to accomodate video media
+      if (data.media_type === 'video') {
+        document.querySelector('iframe').classList.remove('hidden');
+        document.querySelector('iframe').src = data.url;
+      } else {
+        document.querySelector('img').classList.remove('hidden');
+        document.querySelector('img').src = data.hdurl;
+      }
+      document.querySelector('p').innerText = data.explanation;
     })
     .catch((err) => console.log(`Error ${err}`));
 }
