efa705dc96ba834769a714446783c4146feaafde
Anastacia Muringo <anastasmuringo@gmail.com>
2022-07-14 11:43:32 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+//fetch api from nasa servers
+const date = document.querySelector('input').value;
+console.log(date);
+const url = `https://api.nasa.gov/planetary/apod?api_key=AHRztZy0Y5bHCUzxAfEHzLaXfeS7JuBOQYn9wzaq&date=${date}`;
+//When user clicks apod, get the p of that specific date
+//Create event listener
+document.querySelector('button').addEventListener('click', getApod);
+function getApod() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      //create a conditional to accomodate video media
+      if (data.media_type === 'video') {
+        document.querySelector('iframe').classList.remove('hidden');
+        document.querySelector('iframe').src = data.url;
+      } else {
+        document.querySelector('img').classList.remove('hidden');
+        document.querySelector('img').src = data.hdurl;
+      }
+      document.querySelector('p').innerText = data.explanation;
+    })
+    .catch((err) => console.log(`Error ${err}`));
+}
