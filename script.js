edb9686e9f548c2c08bd4f5e08f8887005c93bcc
Anastacia Muringo <anastasmuringo@gmail.com>
2022-07-14 11:56:32 +0300

    Display date on page

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 //fetch api from nasa servers
 const date = document.querySelector('input').value;
-console.log(date);
 const url = `https://api.nasa.gov/planetary/apod?api_key=AHRztZy0Y5bHCUzxAfEHzLaXfeS7JuBOQYn9wzaq&date=${date}`;
 //When user clicks apod, get the p of that specific date
 //Create event listener
@@ -10,7 +9,7 @@ function getApod() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      document.querySelector('span').innerText = data.date;
       //create a conditional to accomodate video media
       if (data.media_type === 'video') {
         document.querySelector('iframe').classList.remove('hidden');
