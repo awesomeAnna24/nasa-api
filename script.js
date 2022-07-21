d298321c3056401208e32910723b8a4cdd17000e
Anastacia Muringo <anastasmuringo@gmail.com>
2022-07-21 12:02:55 +0300

    Fixing date error

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
-//fetch api from nasa servers
-const date = document.querySelector('input').value;
-const url = `https://api.nasa.gov/planetary/apod?api_key=AHRztZy0Y5bHCUzxAfEHzLaXfeS7JuBOQYn9wzaq&date=${date}`;
-//When user clicks apod, get the p of that specific date
+
 //Create event listener
 document.querySelector('button').addEventListener('click', getApod);
 function getApod() {
-  fetch(url)
+  const date = document.querySelector('input').value;
+  //fetch api from nasa servers
+  fetch(
+    //When user clicks apod, get the p of that specific date
+    `https://api.nasa.gov/planetary/apod?api_key=889eoqBR5EpmgDhIYTQ8WmXSk7XTSHzcKKeqOpbk&date=${date}`
+  )
     .then((res) => res.json())
     .then((data) => {
       document.querySelector('span').innerText =
@@ -16,8 +18,9 @@ function getApod() {
         document.querySelector('iframe').classList.remove('hidden');
         document.querySelector('iframe').src = data.url;
       } else {
+        document.querySelector('iframe').classList.add('hidden');
         document.querySelector('img').classList.remove('hidden');
-        document.querySelector('img').src = data.hdurl;
+        document.querySelector('img').src = data.url;
       }
       document.querySelector('p').innerText = data.explanation;
     })
